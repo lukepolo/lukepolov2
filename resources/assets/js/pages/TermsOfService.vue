e0d7fb9541d952ec7746ea15d381e30a748bed1d
Luke Policinski <Luke@LukePOLO.com>
2017-09-10 17:08:55 -0400

    basic prvaciy

---
+++ resources/assets/js/pages/TermsOfService.vue
@@ -0,0 +1,195 @@
+<template>
+    <div class="container">
+        <div class="section--content">
+            <h1>Terms Of Service</h1>
+            <p>
+                LukePOLO ("LukePOLO", “we”, “us” or “our”) agrees to furnish services to the Subscriber (the “Subscriber”, “you” or “your”),subject to the following Terms of Service.
+                Use of LukePOLO's service constitutes acceptance and agreement to LukePOLO's Terms of Service.
+            </p>
+            <p>
+                LukePOLO reserves the right to modify the Terms of Service without notice.
+            </p>
+
+            <h2>
+                Use of Services
+            </h2>
+            <p>
+                You may use our services, provided that you are of legal age to form a binding contract and are not barred from receiving such services under the laws of the United States or other jurisdictions.
+                In order to access our services, you are required to provide current and factual identification, contact, and other information as part of the registration process.
+                You are responsible for the confidentiality of your account information and for all activities that occur under your account.
+                You are solely responsible for all content within your account.
+                You agree to immediately notify LukePOLO of any unauthorized use of your account or any other breach of security.
+                LukePOLO will not be liable for any loss or damage as a result of your failure to provide us with accurate information or to keep your account secure.
+            </p>
+
+            <h2>
+                Acceptable Conduct
+            </h2>
+            <p>
+                You are responsible for the actions of all users of your account and any data that is created, stored, displayed by, or transmitted by your account while using LukePOLO.
+                You will not engage in any activity that interferes with or disrupts LukePOLO's services or networks connected to LukePOLO.
+            </p>
+
+            <h2>
+                Prohibited Usage
+            </h2>
+
+            <p>
+                You agree that any of the below activities are considered prohibited usage and will result in immediate account suspension or cancellation without a refund and the possibility that LukePOLO will impose fees;
+                and/or pursue civil remedies without providing advance notice.
+            </p>
+
+            <ol>
+                <li>
+                    Misuse of System Resources: Intentional misuse of system resources, including but not limited to employing programs that consume excessive network capacity, CPU cycles, or disk IO.
+                </li>
+
+                <li>
+                    Access to Other Computers or Networks without Authorization: Attempting unauthorized and/or illegal access of computers, networks and/or accounts not belonging to party seeking access.
+                    Any act which interferes with the services of another user or network.
+                    Any act relating to the circumvention of security measures.
+                </li>
+
+                <li>
+                    LukePOLO and the services it provides may only be used for lawful purposes.
+                    Transmission, distribution, or storage of any information, data or material in violation of United States or state regulation or law, or by the common law, is prohibited.
+                    This includes, but is not limited to, material protected by copyright, trademark, trade secret, or other intellectual property rights.
+                    LukePOLO's services may not be used to facilitate infringement of these laws in any way.
+                </li>
+
+                <li>
+                    Other Activities viewed as Illegal or Harmful: Engaging in illegal activities or engaging in activities harmful to the operations of LukePOLO or LukePOLO's customers.
+                </li>
+
+                <li>
+                    Providing False Data on any Contract or Application: including fraudulent use of credit card numbers.
+                </li>
+            </ol>
+
+            <h2>
+                Account Cancellation Or Suspension
+            </h2>
+
+            <p>
+                LukePOLO reserves the right to suspend network access to any customer if, in the judgment of the LukePOLO network administrators, the customer's server is the source or target of a violation of any of the other terms of service or for any other reason which LukePOLO chooses.
+                LukePOLO will use reasonable care in notifying the Customer and in resolving the problem in a method resulting in the least amount of service interference.
+                LukePOLO reserves the right to terminate service without notice for continued and repeated violations of the terms of service.
+                If inappropriate activity is detected, all accounts of the Customer in question will be deactivated until an investigation is complete.
+                Prior notification to the Customer is not assured.
+                In extreme cases, law enforcement will be contacted regarding the activity.
+                The customer will not be credited for the time the customer's machines were suspended.
+            </p>
+
+            <p>
+                If at any time it becomes necessary for LukePOLO to cancel a customer's service without cause, LukePOLO will provide 10 days advance notice.
+            </p>
+
+            <p>
+                You may cancel the service at any time by using the "Cancel Subscription" link located on the "My Account" sub-tab of the LukePOLO web application.
+                Cancellation of service does not relieve Subscriber of responsibility for the payment of all accrued charges.
+            </p>
+
+            <h2>
+                Disclosure To Law Enforcement
+            </h2>
+
+            <p>
+                The Terms of Service specifically prohibits the use of our service for illegal activities.
+                Therefore, Subscriber agrees that LukePOLO may disclose any and all subscriber information including assigned IP numbers, account history, account use, etc. to any court who sends us a valid Court Order, without further consent or notification to the Subscriber.
+                In addition, LukePOLO shall have the right to terminate all service set forth in this Agreement.
+            </p>
+
+            <h2>
+                Server Security
+            </h2>
+
+            <p>
+                Subscriber is solely responsible for maintaining the security of Subscriber's servers that are created via the LukePOLO service.
+                LukePOLO shall have no liability for any security breaches or data breaches of Subscriber's servers, even if those servers were initially created and provisioned by LukePOLO.
+                LukePOLO does not guarantee the security of any of Subscriber's servers, even if those servers were initially created and provisioned by LukePOLO.
+            </p>
+
+            <h2>
+                Warranty Disclaimer
+            </h2>
+
+            <p>
+                We have no special relationship with or fiduciary duty to you.
+                You acknowledge that we have no duty to take any action regarding: which Subscribers gain access to the Services; what Content you access via the Services; or how you may interpret or use the Content.
+            </p>
+
+            <p>
+                You release us from all liability for you having acquired or not acquired Content through the Services.
+                We make no representations concerning any Content contained in or accessed through the Services, and we will not be responsible or liable for the accuracy, copyright compliance, or legality of material or Content contained in or accessed through the Services.
+            </p>
+
+            <p>
+                THE SERVICES AND CONTENT ARE PROVIDED “AS IS”, “AS AVAILABLE” AND WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF TITLE, NON-INFRINGEMENT, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE, AND ANY WARRANTIES IMPLIED BY ANY COURSE OF PERFORMANCE OR USAGE OF TRADE, ALL OF WHICH ARE EXPRESSLY DISCLAIMED.
+                WE, AND OUR DIRECTORS, EMPLOYEES, AGENTS, SUPPLIERS, PARTNERS AND CONTENT PROVIDERS DO NOT WARRANT THAT:
+
+                (I) THE SERVICES WILL BE SECURE OR AVAILABLE AT ANY PARTICULAR TIME OR LOCATION;
+                (II) ANY DEFECTS OR ERRORS WILL BE CORRECTED;
+                (III) ANY CONTENT OR SOFTWARE AVAILABLE AT OR THROUGH THE SERVICES IS FREE OF VIRUSES OR OTHER HARMFUL COMPONENTS;
+                OR (IV) THE RESULTS OF USING THE SERVICES WILL MEET YOUR REQUIREMENTS. YOUR USE OF THE SERVICES IS SOLELY AT YOUR OWN RISK.
+            </p>
+
+            <h2>
+                Limitation Of Liability
+            </h2>
+
+            <p>
+                IN NO EVENT SHALL WE, NOR OUR DIRECTORS, EMPLOYEES, AGENTS, PARTNERS, SUPPLIERS OR CONTENT PROVIDERS, BE LIABLE UNDER CONTRACT, TORT, STRICT LIABILITY, NEGLIGENCE OR ANY OTHER LEGAL OR EQUITABLE THEORY WITH RESPECT TO THE SERVICES
+
+                (I) FOR ANY LOST PROFITS, DATA LOSS, DATA THEFT, COST OF PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES, OR SPECIAL, INDIRECT, INCIDENTAL, PUNITIVE, COMPENSATORY OR CONSEQUENTIAL DAMAGES OF ANY KIND WHATSOEVER, SUBSTITUTE GOODS OR SERVICES (HOWEVER ARISING),
+                (II) FOR ANY BUGS, VIRUSES, TROJAN HORSES, OR THE LIKE (REGARDLESS OF THE SOURCE OF ORIGINATION),
+                OR (III) FOR ANY DIRECT DAMAGES IN EXCESS OF (IN THE AGGREGATE) OF FEES PAID TO US FOR THE PARTICULAR SERVICES DURING THE IMMEDIATELY PREVIOUS ONE MONTH PERIOD, EVEN IF LukePOLO HAD BEEN ADVISED OF, KNEW, OR SHOULD HAVE KNOWN, OF THE POSSIBILITY THEREOF. SUBSCRIBER ACKNOWLEDGES THAT THE FEES PAID BY HIM OR HER REFLECT THE ALLOCATION OF RISK SET FORTH IN THIS AGREEMENT AND THAT LukePOLO WOULD NOT ENTER INTO THIS AGREEMENT WITHOUT THESE LIMITATIONS. SUBSCRIBER HEREBY WAIVES ANY AND ALL CLAIMS AGAINST LukePOLO ARISING OUT OF SUBSCRIBER'S PURCHASE OR USE OF THE SERVICES, OR ANY CONDUCT OF LukePOLO’S DIRECTORS, OFFICERS, EMPLOYEES, AGENTS OR REPRESENTATIVES. YOUR SOLE AND EXCLUSIVE RIGHT AND REMEDY IN CASE OF DISSATISFACTION WITH THE SERVICES OR ANY OTHER GRIEVANCE SHALL BE YOUR TERMINATION AND DISCONTINUATION OF ACCESS TO OR USE OF THE SERVICES.
+            </p>
+
+            <h2>
+                Indemnity
+            </h2>
+            <p>
+                LukePOLO wishes to emphasize that in agreeing to the LukePOLO Terms of Service, customer indemnifies LukePOLO for any violation of the Terms of Service that results in loss to LukePOLO or the bringing of any claim against LukePOLO by any third-party.
+                This means that if LukePOLO is sued because of a customer's or a customer of a customer's activity, the customer will pay any damages awarded against LukePOLO, plus all costs and reasonable attorney's fees.
+            </p>
+
+            <h2>
+                Notice
+            </h2>
+
+            <p>
+                You agree that LukePOLO may provide you with notices, including those regarding changes to the Terms of Service, by email, regular mail, or postings on LukePOLO services.
+            </p>
+
+            <h2>
+                Entire Agreement
+            </h2>
+
+            <p>
+                The Terms of Service (including any policies, guidelines or amendments that may be presented to you from time to time) constitute the entire agreement between you and LukePOLO and govern your use of LukePOLO services, superseding any prior agreements between you and LukePOLO for the use of LukePOLO services.
+            </p>
+
+            <h2>
+                Choice Of Law And Forum
+            </h2>
+
+            <p>
+                The Terms of Service and the relationship between you and LukePOLO shall be governed by the laws of the State of Indiana without regard to its conflict of law provisions. You and LukePOLO agree to submit to the personal and exclusive jurisdiction of the courts located within the county of Marion, Indiana.
+            </p>
+
+            <h2>
+                Waiver And Severability Of Terms
+            </h2>
+            <p>
+                The failure of LukePOLO to exercise or enforce any right or provision of the Terms of Service shall not constitute a waiver of such right or provision. If any provision of the Terms of Service is found by a court of competent jurisdiction to be invalid, the parties nevertheless agree that the court should endeavor to give effect to the party's intentions as reflected in the provision, and the other provisions of the Terms of Service remain in full force and effect.
+            </p>
+
+            <h2>
+                Statute Of Limitations
+            </h2>
+            <p>
+                You agree that regardless of any statute or law to the contrary, any claim or cause of action arising out of or related to use of LukePOLO services or the Terms of Service must be filed within one (1) year after such claim or cause of action arose or be forever barred.
+            </p>
+        </div>
+    </div>
+</template>
--- resources/assets/js/pages/routes.js
+++ resources/assets/js/pages/routes.js
@@ -4,7 +4,9 @@ import Blogs from './Blogs/routes'
 
 import Login from './Login.vue'
 import Resume from './Resume.vue'
+import Privacy from './Privacy.vue'
 import PageNotFound from './PageNotFound.vue'
+import TermsOfService from './TermsOfService.vue'
 
 let routes = [
     ... Home,
@@ -15,6 +17,12 @@ let routes = [
     },
     {
         path: '/resume', component: Resume, name : 'resume',
+    },
+    {
+        path: '/privacy', component: Privacy, name : 'privacy',
+    },
+    {
+        path: '/terms-of-service', component: TermsOfService, name : 'terms-of-service',
     },
     {
         path: "*",
