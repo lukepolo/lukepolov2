fb5d104ad1f7354a53236ba40ceff85b0591b7e0
Luke Policinski <luke@lukepolo.com>
2018-11-21 07:56:24 -0500

    Updating resume link

--- resources/assets/js/pages/Resume.vue
+++ resources/assets/js/pages/Resume.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="container">
         <a href="https://www.dropbox.com/s/4q6urgnixx8tdb0/Luke%20Policinski.pdf?dl=1">Download PDF</a>
-        <iframe class="no-border" src="https://docs.google.com/viewer?url=https://www.dropbox.com/s/4q6urgnixx8tdb0/Luke%20Policinski.pdf?dl=1&embedded=true" width="100%" height="1080"></iframe>
+        <iframe class="no-border" src="https://docs.google.com/viewer?url=https://www.dropbox.com/s/zs8zg5twixg03dt/Luke%20Policinski.pdf?dl=1&embedded=true" width="100%" height="1080"></iframe>
     </div>
 </template>
